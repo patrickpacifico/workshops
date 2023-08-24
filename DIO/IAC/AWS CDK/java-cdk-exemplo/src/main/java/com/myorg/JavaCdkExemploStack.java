758167798cbad6602a001e3cda5e5657e8602199
Patrick Pacífico <patrick.pacifico.s@gmail.com>
2023-08-24 18:41:30 -0300

    Add comandos

--- DIO/IAC/AWS CDK/java-cdk-exemplo/src/main/java/com/myorg/JavaCdkExemploStack.java
+++ DIO/IAC/AWS CDK/java-cdk-exemplo/src/main/java/com/myorg/JavaCdkExemploStack.java
@@ -6,6 +6,7 @@ import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.s3.*;
+import software.amazon.awscdk.services.sns.*;
 
 public class JavaCdkExemploStack extends Stack {
     public JavaCdkExemploStack(final Construct scope, final String id) {
@@ -16,13 +17,17 @@ public class JavaCdkExemploStack extends Stack {
         super(scope, id, props);
 
         // The code that defines your stack goes here
-
+        new Queue(this, "QueueTesteDio");
         // example resource
         final Queue queue = Queue.Builder.create(this, "JavaCdkExemploQueue")
                 .visibilityTimeout(Duration.seconds(300))
                 .build();
 
         final Bucket bucket = Bucket.Builder.create(this, "JavaCdkExemploBucket")
-                .build()
+                .build();
+
+        final Topic topic = Topic.Builder.create(this, "Topic")
+                .displayName("Customer subscription topic")
+                .build();
     }
 }
